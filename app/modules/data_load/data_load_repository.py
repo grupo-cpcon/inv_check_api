dd338207d61bda0ed50d43ebb6643bf27249a94f
Pedro Giamarco <pedrodaniel0020@gmail.com>
2025-12-31 17:53:14 -0300

    add db call on data load

--- app/modules/data_load/data_load_repository.py
+++ app/modules/data_load/data_load_repository.py
@@ -2,7 +2,6 @@ from fastapi import Request, UploadFile
 import pandas as pd
 from app.services.excel_services import build_tree_from_df, clean
 
-
 class DataLoadRepository:
     async def create_many(self, request: Request):
         form = await request.form()
@@ -19,7 +18,7 @@ class DataLoadRepository:
             clean(r)
 
         try:
-            result = await request.db_client.insert_many(tree)
+            result = await request.state.db.items.insert_many(tree)
             return {"message": "Dados inseridos com sucesso!", "count": len(tree)}
         except:
             return {"message": "Não foi possível fazer a carga de itens"}
