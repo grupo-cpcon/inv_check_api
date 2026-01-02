7723cab86c9467e7d3863bab0606c57a15500cab
Pedro Giamarco <pedrodaniel0020@gmail.com>
2026-01-02 12:47:00 -0300

    aaa

--- app/modules/item/item_routes.py
+++ app/modules/item/item_routes.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Request
-from app.core.decorators.auth_decorator import no_auth
 from app.modules.item.item_repository import ItemRepository
 
 router = APIRouter(prefix="/item", tags=["Item"])
