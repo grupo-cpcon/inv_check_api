79141a17eb7dd275781b3d3c215a25bd7598f70f
adriangpo <adrian.gustavopo@gmail.com>
2026-01-09 11:33:21 -0300

    Revert "feat: add ignore on AuthMiddleware if app is on "testing" state"
    This reverts commit 976bcf0151104be111877b909718600f98260fc2.

--- app/core/middlewares/auth_middleware.py
+++ app/core/middlewares/auth_middleware.py
@@ -22,9 +22,6 @@ class AuthMiddleware(BaseHTTPMiddleware):
                 detail="Token not provided."
             )
 
-        if request.app.state.testing:
-            return await call_next(request)
-
         token = auth_header.split("Bearer ")[1].strip()
         if not await AuthService().validate_token(token):
             raise HTTPException(
